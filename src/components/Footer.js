5e32f4454a22c5c600b3dbe8ef6cfffa7a77f48e
Divyam878 <divyamgoyal878@gmail.com>
2025-04-21 03:04:56 +0530

    latest changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="footer bg-[#3e34f4] text-white flex items-center justify-between p-4 w-full transition-transform duration-300 ease-in-out">
       <div className="contact-info">
-        <h2 className="text-lg font-bold" style={{ fontFamily: 'RetroPix' }}>Contact</h2>
+        <h2 className="text-3xl font-bold" style={{ fontFamily: 'Tomorrow-Bold' }}>Contact</h2>
         <p className="text-sm">divyamgoyal878@gmail.com<br />+91 8233120760</p>
       </div>
       <div className="copyright-info text-right">
